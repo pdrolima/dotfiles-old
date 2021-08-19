4281a644b6a6d5185ce807790e9e85d39df90edc
Pedro Lima <pedro-octavio@hotmail.com>
2021-08-18 21:03:27 -0300

    Add Hyper Terminal installation

--- app.sh
+++ app.sh
@@ -7,3 +7,9 @@ sudo snap install postman
 sudo snap install 1password
 sudo snap install spotify
 sudo snap install brave
+
+# Install Hyper Terminal
+TMP_DEB="$(mktemp)" &&
+wget -O  "$TMP_DEB" 'https://github.com/vercel/hyper/releases/download/v3.1.2/hyper_3.1.2_amd64.deb' &&
+sudo dpkg -i "$TMP_DEB"
+rm -f "$TMP_DEB"

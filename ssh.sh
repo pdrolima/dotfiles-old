a822666dc254842a3f337bd1d8106feb4964e52a
Pedro Lima <pedro-octavio@hotmail.com>
2021-08-21 12:48:47 -0300

    Fix adding SSH key

--- ssh.sh
+++ ssh.sh
@@ -12,7 +12,7 @@ ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
 eval "$(ssh-agent -s)"
 
-ssh-add -K ~/.ssh/id_ed25519
+ssh-add ~/.ssh/id_ed25519
 
 # Adding your SSH key to your GitHub account
 # https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
